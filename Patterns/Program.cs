ee1a4c4edfa204b05e31e4c62a027df39d5ff3d2
Chaney Fonseca <chaneyfonseca@gmail.com>
2022-04-06 20:02:12 -0500

    1st commit

---
+++ Patterns/Program.cs
@@ -0,0 +1,9 @@
+﻿using Patterns;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var s1 = Singleton.Instance();
+var s2 = Singleton.Instance();
+
+if (s1 == s2) { Console.WriteLine("First instance of Singleton is == second instance of Singleton"); }
---
+++ Patterns/Singleton.cs
@@ -0,0 +1,11 @@
+﻿namespace Patterns
+{
+    public class Singleton
+    {
+        private static readonly Singleton instance = new();
+
+        protected Singleton() { }
+
+        public static Singleton Instance() { return instance; }
+    }
+}
