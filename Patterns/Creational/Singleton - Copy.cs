c48a08041a171bea9943afd4f06f3b9d4896024b
Chaney Fonseca <chaneyfonseca@gmail.com>
2022-04-10 14:59:33 -0500

    Prototype pattern

---
+++ Patterns/Creational/Singleton - Copy.cs
@@ -0,0 +1,39 @@
+﻿namespace Patterns.Creational
+{
+    /// <summary>
+    /// Prototype specifies the kind of objects to create using a prototypical instance, and create new objects by copying this prototype
+    /// </summary>
+    public abstract class Prototype
+    {
+        private string id;
+
+        public Prototype(string id)
+        {
+            this.id = id;
+        }
+        
+        public string Id { get { return id;  } }
+
+        public abstract Prototype Clone();
+    }
+
+    public class ConcreteType1 : Prototype
+    {
+        public ConcreteType1(string id) : base(id) { }
+
+        public override Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();   // shallow copy
+        }
+    }
+
+    public class ConcreteType2 : Prototype
+    {
+        public ConcreteType2(string id) : base(id) { }
+
+        public override Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();   // shallow copy
+        }
+    }
+}
--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -11,6 +11,7 @@ var s2 = Singleton.Instance();
 if (s1 == s2) { Console.WriteLine("First instance of Singleton is == second instance of Singleton"); }
 Console.WriteLine();
 
+
 Console.WriteLine("Structural - Facade");
 Console.WriteLine("======================");
 var facade = new Facade();
@@ -26,5 +27,18 @@ context.Request();
 context.Request();
 context.Request();
 context.Request();
+Console.WriteLine();
+
+
+Console.WriteLine("Creational - Prototype");
+Console.WriteLine("======================");
+var p1 = new ConcreteType1("I");
+var c1 = (ConcreteType1)p1.Clone();
+Console.WriteLine("Cloned: {0}", c1.Id);
+var p2 = new ConcreteType1("II");
+var c2 = (ConcreteType1)p2.Clone();
+Console.WriteLine("Cloned: {0}", c2.Id);
+
+
 
 Console.ReadKey();
